c169d17c67f5c81f03457b483cca1cd77970a465
raineer <delaritaraineer81@gmail.com>
2022-12-15 17:30:38 +0800

    remove gitignore files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000']
+}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world');
+});
+
+app.get('/',(req, res) => {
+    console.log("Got a GET request for the homepage");
+    res.send('Hello GET');
+ })
+
+app.listen(8000, () => {
+    console.log('listening to port 8000')
+});
